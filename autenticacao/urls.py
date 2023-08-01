5065306506f35cea58cc2745743979af644a8e88
Igor Alexandria <igorgomes120297@gmail.com>
2023-08-01 14:18:08 -0300

    New Features.

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('logar/', views.logar, name='logar'),
+    path('sair/', views.sair, name='sair'),
+
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- imobi/settings.py
+++ imobi/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autenticacao',
+    'plataforma',
+    
 ]
 
 MIDDLEWARE = [
@@ -114,3 +118,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
